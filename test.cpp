a433e7a16a3bc28c020a5623f07bd3ac5cc1e873
Brian Nguyen <brng4374@colorado.edu>
2020-09-22 14:50:31 -0600

    unit tests for SubtractN

--- test.cpp
+++ test.cpp
@@ -5,5 +5,14 @@
 #include <algorithm>
 
 TEST_CASE ("SubtractN for ints", "[subN]") {
+    std::vector<int> v{3,5,7,9};
+    int n = 3;
+    std::vector<int> result_v = SubtractN(v, n);
 
+    srand(time(0));
+    int rand_ind = rand() % v.size();
+
+    REQUIRE(result_v.size() == 4);
+    REQUIRE(result_v[0] == 0);
+    REQUIRE(result_v[rand_ind] == v[rand_ind] - n);
 }
